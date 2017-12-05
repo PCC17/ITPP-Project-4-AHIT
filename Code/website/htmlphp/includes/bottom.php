9bfdcb0102ce87d609253751963cb9ab765e41c1
Dominik Arnstorfer <arnstorfer.dominik@gmail.com>
2017-12-05 08:59:30 +0100

    add includes

---
+++ Code/website/htmlphp/includes/bottom.php
@@ -0,0 +1,5 @@
+  <!-- Bootstrap core JavaScript -->
+  <script src="../../dependencies/jquery.min.js"></script>
+  <script src="../../dependencies/popper.min.js"></script>
+  <script src="../../dependencies/bootstrap.min.js"></script>
+</html>
---
+++ Code/website/htmlphp/includes/top.php
@@ -0,0 +1,14 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>passIn</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../dependencies/bootstrap.min.css" rel="stylesheet">
+
+  </head>
