e3103d03f24aa4fba0d0164033925a270bfcab7f
Bernhard Hackl <hacklbernhard0@gmail.com>
2017-12-12 08:51:05 +0100

    test dependencies

--- Code/website/htmlphp/main/navigation/head/index.php
+++ Code/website/htmlphp/main/navigation/head/index.php
@@ -28,6 +28,36 @@ include "../includes/top.php";
   </div>
 </nav>
 
+
+<nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+          <ul class="nav nav-pills flex-column">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Reports</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Analytics</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Export</a>
+            </li>
+          </ul>
+  </nav>
+
+  <div id='container' style="width: 600px;height: 400px;top:50px;left:50px;">     
+            <div id="elem" onmousedown='mydragg.startMoving(this,"container",event);' onmouseup='mydragg.stopMoving("container");' style="width: 200px;height: 100px;">
+                <div style='width:100%;height:100%;padding:10px'>
+                <select id=test>
+                    <option value=1>first
+                    <option value=2>second
+                </select>
+                <INPUT TYPE=text value="123">
+                </div>
+            </div>
+        </div>
+        
 </body>
 
 
--- Code/website/js/navigation.js
+++ Code/website/js/navigation.js
@@ -1,20 +1,43 @@
-$(document).ready(function() {
-    function toggleNavbarMethod() {
-        if ($(window).width() > 768) {
-            $('.navbar .dropdown').on('mouseover', function(){
-                $('.dropdown-toggle', this).trigger('click'); 
-            }).on('mouseout', function(){
-                $('.dropdown-toggle', this).trigger('click').blur();
-            });
-        }
-        else {
-            $('.navbar .dropdown').off('mouseover').off('mouseout');
-        }
-    }
+ var mydragg = function(){
+                return {
+                    move : function(divid,xpos,ypos){
+                        divid.style.left = xpos + 'px';
+                        divid.style.top = ypos + 'px';
+                    },
+                    startMoving : function(divid,container,evt){
+                        evt = evt || window.event;
+                        var posX = evt.clientX,
+                            posY = evt.clientY,
+                        divTop = divid.style.top,
+                        divLeft = divid.style.left,
+                        eWi = parseInt(divid.style.width),
+                        eHe = parseInt(divid.style.height),
+                        cWi = parseInt(document.getElementById(container).style.width),
+                        cHe = parseInt(document.getElementById(container).style.height);
+                        document.getElementById(container).style.cursor='move';
+                        divTop = divTop.replace('px','');
+                        divLeft = divLeft.replace('px','');
+                        var diffX = posX - divLeft,
+                            diffY = posY - divTop;
+                        document.onmousemove = function(evt){
+                            evt = evt || window.event;
+                            var posX = evt.clientX,
+                                posY = evt.clientY,
+                                aX = posX - diffX,
+                                aY = posY - diffY;
+                                if (aX < 0) aX = 0;
+                                if (aY < 0) aY = 0;
+                                if (aX + eWi > cWi) aX = cWi - eWi;
+                                if (aY + eHe > cHe) aY = cHe -eHe;
+                            mydragg.move(divid,aX,aY);
+                        }
+                    },
+                    stopMoving : function(container){
+                        var a = document.createElement('script');
+                        document.getElementById(container).style.cursor='default';
+                        document.onmousemove = function(){}
+                    },
+                }
+            }();
 
-     // toggle navbar hover
-        toggleNavbarMethod();
-        
-        // bind resize event
-        $(window).resize(toggleNavbarMethod);
-});
+        </script>
