1a113e709b7ba94e9ac1f0d5c9f5aa163d61415e
Dominik Arnstorfer <arnstorfer.dominik@gmail.com>
2018-01-16 09:14:14 +0100

    change includes

--- Code/website/includes/bottom.php
+++ Code/website/includes/bottom.php
@@ -1,4 +1,4 @@
   <!-- Bootstrap core JavaScript -->
-  <script src="../../dependencies/jquery.min.js"></script>
-  <script src="../../dependencies/bootstrap.bundle.min.js"></script>
+  <script src="../dependencies/jquery.min.js"></script>
+  <script src="../dependencies/bootstrap.bundle.min.js"></script>
 </html>
--- Code/website/includes/top.php
+++ Code/website/includes/top.php
@@ -9,9 +9,9 @@
     <title>passIn</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="../../dependencies/bootstrap.min.css" rel="stylesheet">
+    <link href="../dependencies/bootstrap.min.css" rel="stylesheet">
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="../../dependencies/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../dependencies/font-awesome/css/font-awesome.min.css">
 
   </head>
