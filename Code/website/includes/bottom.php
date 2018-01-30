53846680ce5cf2ff024d32fea04f5c7ee4bf1d70
Dominik Arnstorfer <arnstorfer.dominik@gmail.com>
2018-01-30 10:17:32 +0100

    revert change

--- Code/website/includes/bottom.php
+++ Code/website/includes/bottom.php
@@ -1,3 +1,3 @@
 </html>
-<script src="../dependencies/bootstrap.bundle.min.js"></script>
 <script src="../dependencies/jquery.min.js"></script>
+<script src="../dependencies/bootstrap.bundle.min.js"></script>
