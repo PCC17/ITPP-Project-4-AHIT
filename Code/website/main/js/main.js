4ce5df0af64b2b142db8031b674f311b1b1d03ee
Bernhard Hackl <31878030+hackl-berni@users.noreply.github.com>
2018-03-20 08:03:11 +0100

    update API connection

--- Code/website/main/js/main.js
+++ Code/website/main/js/main.js
@@ -1,23 +1,35 @@
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+function checkToken()
+{
+    if(!getCookie("token")=="")
+    {
+        $.ajax({ 
+            type: 'GET', 
+            url: 'https://api.iot.lime-tree.eu/checkToken?token='+getCookie("token"), 
+            xhrFields: { withCredentials: true },
+            dataType: 'json',
+            success: function (data) { 
+                console.log("spitzen token");
+                if(data['status'] == "error")
+                {
+                    window.location = "https://iot.lime-tree.eu/index.php";
+                }
+            }
+        });
     }
-    return "";
+    else
+    {
+        window.location = "https://iot.lime-tree.eu/index.php";
+    }
 }
 
-//$.get("http://10.0.0.21:3005/categories?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhODJhZjc4MTczODk4NTE0MDQ0NGUyYyIsImVtYWlsIjoicGF1bHM4NjlraWw3OC5jYW1lcmxvaGVyQGh0bGtyZW1zLmF0IiwiaWF0IjoxNTIwMzE5Njc1LCJleHAiOjE1MjAzMjk3NTV9.-UKTxAVeK6ioitxGPGlejpcsSiMY_Kel1dhRKYfQsKk", function(data){
-//console.log("succes");
-//console.log(data);
-//});
+
+function getCategories()
+{
+$.get("http://10.0.0.21:3005/categories?token="+getCookie("token"), function(data){
+console.log("succes");
+console.log(data);
+});
+}
 
 
 $(document).ready(function () { 
@@ -28,7 +40,7 @@ $(document).ready(function () {
 
 function addCatgory(){
 
-$.post("http://10.0.0.21:3005/category?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhODJhZjc4MTczODk4NTE0MDQ0NGUyYyIsImVtYWlsIjoicGF1bHM4NjlraWw3OC5jYW1lcmxvaGVyQGh0bGtyZW1zLmF0IiwiaWF0IjoxNTIwMzE5Njc1LCJleHAiOjE1MjAzMjk3NTV9.-UKTxAVeK6ioitxGPGlejpcsSiMY_Kel1dhRKYfQsKk", {"name": "tesgfhjt6","order": 0,"passEntry": [{"name": "google","order": 0,"link": "apple.com","image": "bilduno","username": "hans","password":"hans2","notes": "herbert","customFields": [{"key": "1","value": "dflklg"}],"isfavourite": "false"}]})
+$.post("http://10.0.0.21:3005/category?token="+getCookie("token"), {"name": "tesgfhjt6","order": 0,"passEntry": [{"name": "google","order": 0,"link": "apple.com","image": "bilduno","username": "hans","password":"hans2","notes": "herbert","customFields": [{"key": "1","value": "dflklg"}],"isfavourite": "false"}]})
 }
 
 
@@ -42,7 +54,7 @@ $.ajax({
 
     dataType: 'json',
     type: 'PUT',
-    url: 'http://10.0.0.21:3005/category?token=',
+    url: 'http://10.0.0.21:3005/category?token='+getCookie("token"),
     data: {'name': _name, 'newname': _newName, 'neworder': _orderNumber},
     success: function(data)
     {
