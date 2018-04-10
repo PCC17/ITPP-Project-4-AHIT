bf9d6714df51b6413eb74a9422a773185dac8a56
Bernhard Hackl <hacklbernhard0@gmail.com>
2018-04-10 11:27:24 +0200

    update main

--- Code/website/main/htmlphp/body/index.php
+++ Code/website/main/htmlphp/body/index.php
@@ -1,5 +1,31 @@
 <div id="content">
 
+<div id="favCat">
+  <article class="entry">
+  <div class="card" style="width: 15rem;">
+    <div class="card-header">
+      Featured
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Cras justo odio</li>
+      <li class="list-group-item">Dapibus ac facilisis in</li>
+      <li class="list-group-item">Vestibulum at eros</li>
+    </ul>
+    <div class="card-body">
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+  </div>
+  </article>
+
+</div>
+
+
+
 <article class="entry">
 <div class="card" style="width: 15rem;">
   <div class="card-header">
@@ -20,8 +46,6 @@
   </div>
 </div>
 </article>
-
-
 
 <div id="test"></div>
 
